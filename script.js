4d7e3304e1616893406703241be15cae742f902c
Henrik Lagrosen <henrikl@tuta.io>
2020-11-17 17:12:27 +0100

    routing for sort types

--- script.js
+++ script.js
@@ -4,15 +4,34 @@
 
 function calculateOutput(){
     console.log("Started calculation")
-    const {inputHTML, selectedSortTypeRadioButtonId, selectedSortTypeInputVal} = fetchInput()
+    const {inputArea, selectedSortTypeRadioButtonId, selectedSortTypeInputVal} = fetchInput()
+    routeSortTypes(inputArea, selectedSortTypeRadioButtonId, selectedSortTypeInputVal)
+
 }
 
 function fetchInput(){
-    const inputHTML = $("#inputArea").html()
+    const inputArea = $("#inputArea")
 
     const selectedSortTypeRadioButtonId = $('input[name="sortTypeRadio"]:checked').attr("id")
     const selectedSortTypeInputId = selectedSortTypeRadioButtonId + "Input"
     const selectedSortTypeInputVal = $("#" + selectedSortTypeInputId).val()
 
-    return {inputHTML, selectedSortTypeRadioButtonId, selectedSortTypeInputVal}
+    return {inputArea, selectedSortTypeRadioButtonId, selectedSortTypeInputVal}
+}
+
+function routeSortTypes(inputArea, selectedSortTypeRadioButtonId, selectedSortTypeInputVal){
+    switch(selectedSortTypeRadioButtonId){
+        case "separator":
+            break
+        case "tag":
+            sortByTag(inputArea, selectedSortTypeInputVal)
+            break
+        default:
+            console.log("Err: " + selectedSortTypeRadioButtonId)
+    }
+}
+
+function sortByTag(inputArea, selectedSortTypeInputVal){
+    const matchingTags = inputArea.find(selectedSortTypeInputVal)
+    console.log(matchingTags)
 }
