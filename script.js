f28441e3a3cb939bd21612ff14a2150efa155d7a
Henrik Lagrosen <henrikl@tuta.io>
2020-11-17 15:44:08 +0100

    adds files

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+function calculateOutput(){
+    console.log("Started calculation")
+    fetchInput()
+}
+
+function fetchInput(){
+    const inputHTML = $("#inputArea").html()
+
+    console.log(inputHTML)
+}
